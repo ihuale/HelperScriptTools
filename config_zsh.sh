a114e809284834e36113f8fce3a21b3f3c706cfe
ihuale <ihuale@foxmail.com>
2021-05-06 11:36:01 +0800

    update ohmyzsh scripts

--- config_zsh.sh
+++ config_zsh.sh
@@ -5,8 +5,9 @@ sudo apt -y install zsh git wget
 echo "start to install ohmyzsh..."
 if [ ! -d ohmyzsh ]; then
     git clone https://github.com.cnpmjs.org/ohmyzsh/ohmyzsh.git && chmod +x ohmyzsh/tools/install_ohmyzsh.sh && ./ohmyzsh/tools/install.sh
+    rm -rfv ohmyzsh
 fi
-rm -rfv ohmyzsh
+
 
 echo "start to install antigen for ohmyzsh..."
 ANTIGEN_INSTALL_DIR="/home/hl/.oh-my-zsh/tools"
