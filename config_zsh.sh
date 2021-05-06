ed3fc6fff22088c9a6f437e68fee0bd9004df7ba
ihuale <ihuale@foxmail.com>
2021-05-06 11:32:27 +0800

    add install onmyzsh support

---
+++ config_zsh.sh
@@ -0,0 +1,23 @@
+set -e
+
+sudo apt -y install zsh git wget
+
+echo "start to install ohmyzsh..."
+if [ ! -d ohmyzsh ]; then
+    git clone https://github.com.cnpmjs.org/ohmyzsh/ohmyzsh.git && chmod +x ohmyzsh/tools/install_ohmyzsh.sh && ./ohmyzsh/tools/install.sh
+fi
+rm -rfv ohmyzsh
+
+echo "start to install antigen for ohmyzsh..."
+ANTIGEN_INSTALL_DIR="/home/hl/.oh-my-zsh/tools"
+if [ ! -d ${ANTIGEN_INSTALL_DIR} ]; then
+    echo "${ANTIGEN_INSTALL_DIR} not exist, so make it..."
+    mkdir -p ${ANTIGEN_INSTALL_DIR}
+fi
+# rm -rfv ${ANTIGEN_INSTALL_DIR}/antigen
+git clone https://github.com.cnpmjs.org/zsh-users/antigen.git ${ANTIGEN_INSTALL_DIR}/antigen
+echo "antigen install over..."
+
+cp -v ./.zshrc ~
+
+chsh -s /bin/zsh
